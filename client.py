7d961e6ba2a5816b4e423784c0f5d2cea37d5e72
Tomki2258 <52979692+Tomki2258@users.noreply.github.com>
2022-12-04 20:01:41 +0100

    start

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "Client IP"
+PORT = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.sendall(data)
+    s.close()
